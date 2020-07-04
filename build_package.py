265d4cff070d2d4f6e2565a49ce14dde7f75aa78
Pavel Feldman <pavel.feldman@gmail.com>
2020-07-04 10:14:47 -0700

    chore: bump version to 0.0.4

---
+++ build_package.py
@@ -0,0 +1,27 @@
+# Copyright (c) Microsoft Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import shutil
+import subprocess
+
+folder = os.path.dirname(os.path.abspath(__file__))
+if os.path.exists(os.path.join(folder, 'build')):
+  shutil.rmtree(os.path.join(folder, 'build'))
+if os.path.exists(os.path.join(folder, 'dist')):
+  shutil.rmtree(os.path.join(folder, 'dist'))
+if os.path.exists(os.path.join(folder, 'playwright_web.egg-info')):
+  shutil.rmtree(os.path.join(folder, 'playwright_web.egg-info'))
+
+subprocess.run(['python3', 'setup.py', 'sdist', 'bdist_wheel'])
