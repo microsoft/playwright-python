80dad4ab73b4cfe9cec7c13948c3aef33433f758
Max Schmitt <max@schmitt.mx>
2020-07-24 15:09:11 +0200

    chore(build-driver): auto update README on roll

--- build_driver.py
+++ build_driver.py
@@ -14,33 +14,52 @@
 
 import gzip
 import os
+import re
 import shutil
 import subprocess
+from pathlib import Path
 
-driver_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "driver")
-package_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "playwright")
-drivers_path = os.path.join(package_path, "drivers")
+_dirname = Path(os.path.dirname(os.path.abspath(__file__)))
 
-if os.path.exists(os.path.join(driver_path, "package-lock.json")):
-    os.remove(os.path.join(driver_path, "package-lock.json"))
-if os.path.exists(os.path.join(driver_path, "node_modules")):
-    shutil.rmtree(os.path.join(driver_path, "node_modules"))
-if os.path.exists(os.path.join(driver_path, "out")):
-    shutil.rmtree(os.path.join(driver_path, "out"))
+driver_path = _dirname / "driver"
+package_path = _dirname / "playwright"
+drivers_path = package_path / "drivers"
+
+if (driver_path / "package-lock.json").exists():
+    os.remove(driver_path / "package-lock.json")
+if (driver_path / "node_modules").exists():
+    shutil.rmtree(driver_path / "node_modules")
+if (driver_path / "out").exists():
+    shutil.rmtree(driver_path / "out")
 
 subprocess.run("npm i", cwd=driver_path, shell=True)
 subprocess.run("npm run bake", cwd=driver_path, shell=True)
 
 for driver in ["driver-linux", "driver-macos", "driver-win.exe"]:
-    if os.path.exists(os.path.join(package_path, driver)):
-        os.remove(os.path.join(package_path, driver))
+    if (package_path / driver).exists():
+        os.remove((package_path / driver))
 
-    in_path = os.path.join(driver_path, "out", driver)
-    out_path = os.path.join(drivers_path, driver + ".gz")
+    in_path = driver_path / "out" / driver
+    out_path = drivers_path / (driver + ".gz")
     with open(in_path, "rb") as f_in, gzip.open(out_path, "wb") as f_out:
         shutil.copyfileobj(f_in, f_out)
 
+node_modules_playwright = driver_path / "node_modules" / "playwright"
+
 shutil.copyfile(
-    os.path.join(driver_path, "node_modules", "playwright", "browsers.json"),
-    os.path.join(drivers_path, "browsers.json"),
+    node_modules_playwright / "browsers.json", drivers_path / "browsers.json",
 )
+
+upstream_readme = (node_modules_playwright / "README.md").read_text()
+pw_python_readme = (_dirname / "README.md").read_text()
+
+matches = re.findall(r"<!-- GEN:(.*?) -->(.*?)<!-- GEN:stop -->", upstream_readme)
+
+for key, value in matches:
+    pw_python_readme = re.sub(
+        rf"(<!-- GEN:{key} -->).*?(<!-- GEN:stop -->)",
+        f"<!-- GEN:{key} -->{value}<!-- GEN:stop -->",
+        pw_python_readme,
+    )
+
+(_dirname / "README.md").write_text(pw_python_readme)
