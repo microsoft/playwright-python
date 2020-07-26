917fddb5adf08dbd68e5ad5613f877176b5ab79a
Max Schmitt <max@schmitt.mx>
2020-07-27 01:32:38 +0200

    fix generate script test

--- tests/test_generate_sync_api.py
+++ tests/test_generate_sync_api.py
@@ -1,9 +1,13 @@
+import sys
 from io import StringIO
 from unittest.mock import patch
+
+import pytest
 
 from scripts.generate_sync_api import main
 
 
+@pytest.mark.skipif(sys.version_info < (3, 8), reason="requires python3.8 or higher")
 def test_generate_sync_api():
     with patch("sys.stdout", new_callable=StringIO):
         main()
