8272826cc774b18c789b437382175212a57b1e12
Pavel <pavel.feldman@gmail.com>
2020-07-28 12:01:37 -0700

    feat(install): introduce explict post-installation step

--- playwright/main.py
+++ playwright/main.py
@@ -25,37 +25,36 @@ from greenlet import greenlet
 
 from playwright.async_api import Playwright as AsyncPlaywright
 from playwright.connection import Connection
+from playwright.helper import not_installed_error
 from playwright.object_factory import create_remote_object
 from playwright.playwright import Playwright
 from playwright.sync_api import Playwright as SyncPlaywright
 from playwright.sync_base import dispatcher_fiber, set_dispatcher_fiber
 
 
-async def run_driver_async() -> Connection:
-    package_path = os.path.dirname(os.path.abspath(__file__))
+def compute_driver_name() -> str:
     platform = sys.platform
     if platform == "darwin":
-        driver_name = "driver-macos"
+        result = "driver-macos"
     elif platform == "linux":
-        driver_name = "driver-linux"
+        result = "driver-linux"
     elif platform == "win32":
-        driver_name = "driver-win.exe"
+        result = "driver-win.exe"
+    return result
+
+
+async def run_driver_async() -> Connection:
+    package_path = os.path.dirname(os.path.abspath(__file__))
+    driver_name = compute_driver_name()
     driver_executable = os.path.join(package_path, driver_name)
     archive_name = os.path.join(package_path, "drivers", driver_name + ".gz")
 
     if not os.path.exists(driver_executable) or os.path.getmtime(
         driver_executable
     ) < os.path.getmtime(archive_name):
-        with gzip.open(archive_name, "rb") as f_in, open(
-            driver_executable, "wb"
-        ) as f_out:
-            shutil.copyfileobj(f_in, f_out)
-
-    st = os.stat(driver_executable)
-    if st.st_mode & stat.S_IEXEC == 0:
-        os.chmod(driver_executable, st.st_mode | stat.S_IEXEC)
-
-    subprocess.run(f"{driver_executable} install", shell=True)
+        raise not_installed_error(
+            "Playwright requires additional post-installation step to be made."
+        )
 
     proc = await asyncio.create_subprocess_exec(
         driver_executable,
@@ -116,3 +115,32 @@ if sys.platform == "win32":
     # Use ProactorEventLoop in 3.7, which is default in 3.8
     loop = asyncio.ProactorEventLoop()
     asyncio.set_event_loop(loop)
+
+
+def main() -> None:
+    if "install" not in sys.argv:
+        print('Run "python -m playwright install" to complete installation')
+        return
+    package_path = os.path.dirname(os.path.abspath(__file__))
+    driver_name = compute_driver_name()
+    driver_executable = os.path.join(package_path, driver_name)
+    archive_name = os.path.join(package_path, "drivers", driver_name + ".gz")
+
+    if not os.path.exists(driver_executable) or os.path.getmtime(
+        driver_executable
+    ) < os.path.getmtime(archive_name):
+        print(f"Extracting {archive_name} into {driver_executable}...")
+        with gzip.open(archive_name, "rb") as f_in, open(
+            driver_executable, "wb"
+        ) as f_out:
+            shutil.copyfileobj(f_in, f_out)
+
+        st = os.stat(driver_executable)
+        if st.st_mode & stat.S_IEXEC == 0:
+            print(f"Making {driver_executable} executable...")
+            os.chmod(driver_executable, st.st_mode | stat.S_IEXEC)
+
+    print("Installing the browsers...")
+    subprocess.run(f"{driver_executable} install", shell=True)
+
+    print("Playwright is now ready for use")
