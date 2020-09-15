542c3a7e5e5f42ed9d802a309cf0f19f1b0b80d6
Max Schmitt <max@schmitt.mx>
2020-09-16 00:29:12 +0200

    docs: adjust style of the landing page (#191)

--- playwright/__init__.py
+++ playwright/__init__.py
@@ -11,6 +11,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""
+Python package `playwright` is a Python library to automate Chromium,
+Firefox and WebKit with a single API. Playwright is built to enable cross-browser
+web automation that is ever-green, capable, reliable and fast.
+For more information you'll find the documentation for the sync API [here](sync_api.html)
+and for the async API [here](async_api.html).
+"""
 
 import playwright.helper as helper
 from playwright.main import AsyncPlaywrightContextManager, SyncPlaywrightContextManager
@@ -33,3 +40,37 @@ __all__ = [
     "Error",
     "TimeoutError",
 ]
+
+__pdoc__ = {
+    "accessibility": False,
+    "async_base": False,
+    "browser": False,
+    "browser_context": False,
+    "browser_type": False,
+    "cdp_session": False,
+    "chromium_browser_context": False,
+    "connection": False,
+    "console_message": False,
+    "dialog": False,
+    "download": False,
+    "element_handle": False,
+    "event_context_manager": False,
+    "file_chooser": False,
+    "frame": False,
+    "helper": False,
+    "impl_to_api_mapping": False,
+    "input": False,
+    "js_handle": False,
+    "main": False,
+    "network": False,
+    "object_factory": False,
+    "page": False,
+    "path_utils": False,
+    "playwright": False,
+    "selectors": False,
+    "sync_base": False,
+    "transport": False,
+    "wait_helper": False,
+    "async_playwright": False,
+    "sync_playwright": False,
+}
