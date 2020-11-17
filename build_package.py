cb7abd52fcd89fbe52a849eb1dc16dbb1546ed3e
Kumar Aditya <59607654+kumaraditya303@users.noreply.github.com>
2020-11-18 03:03:47 +0530

    fix: Use urllib instead of curl (#302)

--- build_package.py
+++ build_package.py
@@ -18,6 +18,7 @@ import shutil
 import stat
 import subprocess
 import sys
+import urllib.request
 import zipfile
 
 from playwright.path_utils import get_file_dirname
@@ -40,7 +41,7 @@ for platform in ["mac", "linux", "win32", "win32_x64"]:
     if not os.path.exists("driver/" + zip_file):
         url = "https://playwright.azureedge.net/builds/cli/next/" + zip_file
         print("Fetching ", url)
-        subprocess.check_call(["curl", url, "-o", "driver/" + zip_file])
+        urllib.request.urlretrieve(url, "driver/" + zip_file)
 
 _dirname = get_file_dirname()
 _build_dir = _dirname / "build"
