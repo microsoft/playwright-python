125b4a3474d131b2b5bdbbbb7cec7582752ed325
Pavel Feldman <pavel.feldman@gmail.com>
2020-11-18 14:07:13 -0800

    chore: update readme upon api update

---
+++ scripts/update_versions.py
@@ -0,0 +1,15 @@
+import re
+from playwright import sync_playwright
+
+with sync_playwright() as p:
+    r = open("README.md", "r")
+    text = r.read()
+    for browser_type in [p.chromium, p.firefox, p.webkit]:
+        rx = re.compile(r"<!-- GEN:" + browser_type.name + r"-version -->([^<]+)<!-- GEN:stop -->")
+        browser = browser_type.launch()
+        text = rx.sub(f"<!-- GEN:{browser_type.name}-version -->{browser.version}<!-- GEN:stop -->", text)
+        browser.close()
+
+    w = open("README.md", "w")
+    w.write(text)
+    w.close()
