1dba42204d0cbd75dcd2856cd0f4e9d5365734e7
Pavel Feldman <pavel.feldman@gmail.com>
2020-11-21 20:02:49 -0800

    lint: fix violations

--- scripts/update_versions.py
+++ scripts/update_versions.py
@@ -1,13 +1,19 @@
 import re
+
 from playwright import sync_playwright
 
 with sync_playwright() as p:
     r = open("README.md", "r")
     text = r.read()
     for browser_type in [p.chromium, p.firefox, p.webkit]:
-        rx = re.compile(r"<!-- GEN:" + browser_type.name + r"-version -->([^<]+)<!-- GEN:stop -->")
+        rx = re.compile(
+            r"<!-- GEN:" + browser_type.name + r"-version -->([^<]+)<!-- GEN:stop -->"
+        )
         browser = browser_type.launch()
-        text = rx.sub(f"<!-- GEN:{browser_type.name}-version -->{browser.version}<!-- GEN:stop -->", text)
+        text = rx.sub(
+            f"<!-- GEN:{browser_type.name}-version -->{browser.version}<!-- GEN:stop -->",
+            text,
+        )
         browser.close()
 
     w = open("README.md", "w")
