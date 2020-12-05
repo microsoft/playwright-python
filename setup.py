c5378b1576e9c8b1727cde4bb618af2752aeef29
Kumar Aditya <rahuladitya303@gmail.com>
2020-12-05 18:04:09 +0530

    chore: Use bdist_wheel instead of custom script

--- setup.py
+++ setup.py
@@ -12,10 +12,86 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import glob
+import os
+import shutil
+import stat
+import subprocess
+import sys
+import zipfile
+
 import setuptools
+from wheel.bdist_wheel import bdist_wheel as BDistWheelCommand
+
+driver_version = "0.170.0-next.1605573954344"
+
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
+
+
+class PlaywrightBDistWheelCommand(BDistWheelCommand):
+    def run(self) -> None:
+        if os.path.exists("build"):
+            shutil.rmtree("build")
+        if os.path.exists("dist"):
+            shutil.rmtree("dist")
+        if os.path.exists("playwright.egg-info"):
+            shutil.rmtree("playwright.egg-info")
+        super().run()
+        os.makedirs("driver", exist_ok=True)
+        os.makedirs("playwright/driver", exist_ok=True)
+        for platform in ["mac", "linux", "win32", "win32_x64"]:
+            zip_file = f"playwright-cli-{driver_version}-{platform}.zip"
+            if not os.path.exists("driver/" + zip_file):
+                url = "https://playwright.azureedge.net/builds/cli/next/" + zip_file
+                print("Fetching ", url)
+                subprocess.check_call(
+                    ["curl", "--http1.1", url, "-o", "driver/" + zip_file]
+                )
+        base_wheel_location = glob.glob("dist/*.whl")[0]
+        without_platform = base_wheel_location[:-7]
+        platform_map = {
+            "darwin": "mac",
+            "linux": "linux",
+            "win32": "win32_x64" if sys.maxsize > 2 ** 32 else "win32",
+        }
+        for platform in ["mac", "linux", "win32", "win32_x64"]:
+            zip_file = f"driver/playwright-cli-{driver_version}-{platform}.zip"
+            with zipfile.ZipFile(zip_file, "r") as zip:
+                zip.extractall(f"driver/{platform}")
+            if platform_map[sys.platform] == platform:
+                with zipfile.ZipFile(zip_file, "r") as zip:
+                    zip.extractall("playwright/driver")
+                for file in os.listdir("playwright/driver"):
+                    if file == "playwright-cli" or file.startswith("ffmpeg"):
+                        print(f"playwright/driver/{file}")
+                        os.chmod(
+                            f"playwright/driver/{file}",
+                            os.stat(f"playwright/driver/{file}").st_mode | stat.S_IEXEC,
+                        )
+            wheel = ""
+            if platform == "mac":
+                wheel = "macosx_10_13_x86_64.whl"
+            if platform == "linux":
+                wheel = "manylinux1_x86_64.whl"
+            if platform == "win32":
+                wheel = "win32.whl"
+            if platform == "win32_x64":
+                wheel = "win_amd64.whl"
+            wheel_location = without_platform + wheel
+            shutil.copy(base_wheel_location, wheel_location)
+            with zipfile.ZipFile(wheel_location, "a") as zip:
+                for file in os.listdir(f"driver/{platform}"):
+                    from_location = f"driver/{platform}/{file}"
+                    to_location = f"playwright/driver/{file}"
+                    if file == "playwright-cli" or file.startswith("ffmpeg"):
+                        os.chmod(
+                            from_location, os.stat(from_location).st_mode | stat.S_IEXEC
+                        )
+                    zip.write(from_location, to_location)
+        os.remove(base_wheel_location)
+
 
 setuptools.setup(
     name="playwright",
@@ -40,6 +116,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.7",
+    cmdclass={"bdist_wheel": PlaywrightBDistWheelCommand},
     use_scm_version={
         "version_scheme": "post-release",
         "write_to": "playwright/_repo_version.py",
