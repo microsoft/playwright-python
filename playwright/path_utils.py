1936c529b67d718940bfdc6d872f0181518d912c
tumregels <lev@valital.com>
2020-12-10 11:43:49 -0500

    fix compute_driver_executable (fix #342)

--- playwright/path_utils.py
+++ playwright/path_utils.py
@@ -13,14 +13,12 @@
 # limitations under the License.
 
 import inspect
-import sys
 from pathlib import Path
 
 
 def get_file_dirname() -> Path:
     """Returns the callee (`__file__`) directory name"""
-    frame = inspect.currentframe()
-    module_name = frame.f_back.f_globals["__name__"]
-    module = sys.modules[module_name]
+    frame = inspect.stack()[1]
+    module = inspect.getmodule(frame[0])
     assert module
     return Path(module.__file__).parent.absolute()
