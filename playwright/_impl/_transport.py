e641ee8a711a735e46f54e084490528d9dc21b70
Max Schmitt <max@schmitt.mx>
2020-12-27 22:59:44 +0100

    fix(driver): crash with VSCode debug terminal

--- playwright/_impl/_transport.py
+++ playwright/_impl/_transport.py
@@ -48,11 +48,15 @@ class Transport:
 
     async def run(self) -> None:
         self._loop = asyncio.get_running_loop()
-        driver_executable = self._driver_executable
+
+        driver_env = os.environ.copy()
+        # VSCode's JavaScript Debug Terminal provides it but driver/pkg does not support it
+        driver_env.pop("NODE_OPTIONS", None)
 
         proc = await asyncio.create_subprocess_exec(
-            str(driver_executable),
+            str(self._driver_executable),
             "run-driver",
+            env=driver_env,
             stdin=asyncio.subprocess.PIPE,
             stdout=asyncio.subprocess.PIPE,
             stderr=_get_stderr_fileno(),
