73d1a5af14e0d65a947ec61adaa9ec5a04ea66f6
Kumar Aditya <59607654+kumaraditya303@users.noreply.github.com>
2021-02-13 18:05:22 +0530

    build: Remove redundant build files

--- scripts/postPdoc3Generation.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-// Removes the sidebar on the landing page (/)
-const indexPath = 'htmldocs/playwright/index.html';
-let input = fs.readFileSync(indexPath).toString();
-input = input.replace(/<nav id="sidebar">.*<\/nav>/s, '');
-fs.writeFileSync(indexPath, input);
--- scripts/update_versions.py
+++ scripts/update_versions.py
@@ -1,21 +1,27 @@
 import re
+from pathlib import Path
 
 from playwright.sync_api import sync_playwright
 
-with sync_playwright() as p:
-    r = open("README.md", "r")
-    text = r.read()
-    for browser_type in [p.chromium, p.firefox, p.webkit]:
-        rx = re.compile(
-            r"<!-- GEN:" + browser_type.name + r"-version -->([^<]+)<!-- GEN:stop -->"
-        )
-        browser = browser_type.launch()
-        text = rx.sub(
-            f"<!-- GEN:{browser_type.name}-version -->{browser.version}<!-- GEN:stop -->",
-            text,
-        )
-        browser.close()
 
-    w = open("README.md", "w")
-    w.write(text)
-    w.close()
+def main() -> None:
+    with sync_playwright() as p:
+        readme = Path("README.md").resolve()
+        text = readme.read_text(encoding="utf-8")
+        for browser_type in [p.chromium, p.firefox, p.webkit]:
+            rx = re.compile(
+                r"<!-- GEN:"
+                + browser_type.name
+                + r"-version -->([^<]+)<!-- GEN:stop -->"
+            )
+            browser = browser_type.launch()
+            text = rx.sub(
+                f"<!-- GEN:{browser_type.name}-version -->{browser.version}<!-- GEN:stop -->",
+                text,
+            )
+            browser.close()
+        readme.write_text(text, encoding="utf-8")
+
+
+if __name__ == "__main__":
+    main()
