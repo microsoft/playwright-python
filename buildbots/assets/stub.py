463c6dffe1044e95db5c52b7e4872b2092490748
Kumar Aditya <59607654+kumaraditya303@users.noreply.github.com>
2021-04-16 14:15:19 +0530

    chore: simplified tests (#558)

--- buildbots/assets/stub.py
+++
@@ -1,9 +0,0 @@
-from playwright.sync_api import sync_playwright
-
-with sync_playwright() as p:
-    for browser_type in [p.chromium, p.firefox, p.webkit]:
-        browser = browser_type.launch()
-        page = browser.new_page()
-        page.set_content("<h1>Test 123</h1>")
-        page.screenshot(path=f"{browser_type.name}.png")
-        browser.close()
