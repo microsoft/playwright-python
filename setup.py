5133469433d7c9a68696c6885fb1113bece431e1
Andrey Lushnikov <lushnikov@chromium.org>
2021-09-30 21:17:42 +0300

    chore(release-1.15): roll driver to 1.15.0-1633020276000

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ except ImportError:
     InWheel = None
 from wheel.bdist_wheel import bdist_wheel as BDistWheelCommand
 
-driver_version = "1.15.0-1632336523000"
+driver_version = "1.15.0-1633020276000"
 
 
 def extractall(zip: zipfile.ZipFile, path: str) -> None:
