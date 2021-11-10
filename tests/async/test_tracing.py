b5ef6241db369101044512867f25799e8a52e845
Max Schmitt <max@schmitt.mx>
2021-11-10 17:34:44 +0100

    test: add tracing related tests (#1020)

--- tests/async/test_tracing.py
+++ tests/async/test_tracing.py
@@ -14,11 +14,42 @@
 
 from pathlib import Path
 
+from playwright.async_api import Browser, BrowserContext
+from tests.server import Server
 
-async def test_browser_context_output_trace(browser, server, tmp_path):
+
+async def test_browser_context_output_trace(
+    browser: Browser, server: Server, tmp_path: Path
+):
     context = await browser.new_context()
     await context.tracing.start(screenshots=True, snapshots=True)
     page = await context.new_page()
     await page.goto(server.PREFIX + "/grid.html")
     await context.tracing.stop(path=tmp_path / "trace.zip")
     assert Path(tmp_path / "trace.zip").exists()
+
+
+async def test_browser_context_should_not_throw_when_stopping_without_start_but_not_exporting(
+    context: BrowserContext, server: Server, tmp_path: Path
+):
+    await context.tracing.stop()
+
+
+async def test_browser_context_output_trace_chunk(
+    browser: Browser, server: Server, tmp_path: Path
+):
+    context = await browser.new_context()
+    await context.tracing.start(screenshots=True, snapshots=True)
+    page = await context.new_page()
+    await page.goto(server.PREFIX + "/grid.html")
+    button = page.locator(".box").first
+
+    await context.tracing.start_chunk(title="foo")
+    await button.click()
+    await context.tracing.stop_chunk(path=tmp_path / "trace1.zip")
+    assert Path(tmp_path / "trace1.zip").exists()
+
+    await context.tracing.start_chunk(title="foo")
+    await button.click()
+    await context.tracing.stop_chunk(path=tmp_path / "trace2.zip")
+    assert Path(tmp_path / "trace2.zip").exists()
