69fc42fbf8ab377bc9376262bee316441c768b23
Kumar Aditya <59607654+kumaraditya303@users.noreply.github.com>
2021-11-11 13:14:57 +0530

    fix pyinstaller

--- playwright/_impl/_transport.py
+++ playwright/_impl/_transport.py
@@ -65,7 +65,7 @@ class PipeTransport:
         # For pyinstaller
         env = os.environ.copy()
         if getattr(sys, "frozen", False):
-            env["PLAYWRIGHT_BROWSERS_PATH"] = "0"
+            env.setdefault("PLAYWRIGHT_BROWSERS_PATH", "0")
         self._proc = await asyncio.create_subprocess_exec(
             str(self._driver_executable),
             "run-driver",
