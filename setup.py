1003d082057374385c9a608ecae4b50d4715db97
Andrey Lushnikov <aslushnikov@gmail.com>
2022-01-19 17:47:25 -0700

    chore: roll to latest 1.18.0 driver (#1114)

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ except ImportError:
     InWheel = None
 from wheel.bdist_wheel import bdist_wheel as BDistWheelCommand
 
-driver_version = "1.18.0-beta-1642508722000"
+driver_version = "1.18.0-beta-1642620709000"
 
 
 def extractall(zip: zipfile.ZipFile, path: str) -> None:
