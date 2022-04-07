bfb2869d9f403a5272ce5d0be03472ac333392bf
Max Schmitt <max@schmitt.mx>
2022-04-07 05:36:45 +0200

    fix: forward cancelled tasks to the inner task (#1236)
    * fix: forward cancelled tasks to the inner task
    
    * rev2

---
+++ tests/async/test_asyncio.py
@@ -0,0 +1,50 @@
+# Copyright (c) Microsoft Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License")
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import asyncio
+import gc
+from typing import Dict
+
+import pytest
+
+from playwright.async_api import async_playwright
+
+
+async def test_should_cancel_underlying_protocol_calls(
+    browser_name: str, launch_arguments: Dict
+):
+    handler_exception = None
+
+    def exception_handlerdler(loop, context) -> None:
+        nonlocal handler_exception
+        handler_exception = context["exception"]
+
+    asyncio.get_running_loop().set_exception_handler(exception_handlerdler)
+
+    async with async_playwright() as p:
+        browser = await p[browser_name].launch(**launch_arguments)
+        page = await browser.new_page()
+        task = asyncio.create_task(page.wait_for_selector("will-never-find"))
+        # make sure that the wait_for_selector message was sent to the server (driver)
+        await asyncio.sleep(0.1)
+        task.cancel()
+        with pytest.raises(asyncio.CancelledError):
+            await task
+        await browser.close()
+
+    # The actual 'Future exception was never retrieved' is logged inside the Future destructor (__del__).
+    gc.collect()
+
+    assert handler_exception is None
+
+    asyncio.get_running_loop().set_exception_handler(None)
