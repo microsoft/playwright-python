a3a1766817b58c6d3bb29fd52b937414d4372d1c
Ross A. Wollman <ross.wollman@gmail.com>
2022-05-11 14:40:37 -0700

    add missing impl

---
+++ playwright/_impl/_map.py
@@ -0,0 +1,20 @@
+from typing import Dict, Generic, Tuple, TypeVar
+
+K = TypeVar("K")
+V = TypeVar("V")
+
+
+class Map(Generic[K, V]):
+    def __init__(self) -> None:
+        self._entries: Dict[int, Tuple[K, V]] = {}
+        self._last_id = 0
+
+    def __contains__(self, item: K) -> bool:
+        return id(item) in self._entries
+
+    def __setitem__(self, idx: K, value: V) -> None:
+        self._last_id += 1
+        self._entries[id(idx)] = (idx, value)
+
+    def __getitem__(self, obj: K) -> V:
+        return self._entries[id(obj)][1]
