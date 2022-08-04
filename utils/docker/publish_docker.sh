ab6bea1ef1c2358440835b866b98bb5b479139e3
Ross Wollman <rwoll@users.noreply.github.com>
2022-08-04 14:50:47 -0700

    chore: publish official jammy image (#1477)

--- utils/docker/publish_docker.sh
+++ utils/docker/publish_docker.sh
@@ -44,6 +44,13 @@ FOCAL_TAGS=(
   "next-focal"
 )
 
+JAMMY_TAGS=(
+  "next-jammy"
+)
+if [[ "$RELEASE_CHANNEL" == "stable" ]]; then
+  JAMMY_TAGS+=("jammy")
+fi
+
 if [[ "$RELEASE_CHANNEL" == "stable" ]]; then
   FOCAL_TAGS+=("latest")
   FOCAL_TAGS+=("focal")
@@ -66,8 +73,10 @@ publish_docker_images_with_arch_suffix() {
     TAGS=("${BIONIC_TAGS[@]}")
   elif [[ "$FLAVOR" == "focal" ]]; then
     TAGS=("${FOCAL_TAGS[@]}")
+  elif [[ "$FLAVOR" == "jammy" ]]; then
+    TAGS=("${JAMMY_TAGS[@]}")
   else
-    echo "ERROR: unknown flavor - $FLAVOR. Must be either 'bionic' or 'focal'"
+    echo "ERROR: unknown flavor - $FLAVOR. Must be either 'bionic', 'focal', or 'jammy'"
     exit 1
   fi
   local ARCH="$2"
@@ -92,8 +101,10 @@ publish_docker_manifest () {
     TAGS=("${BIONIC_TAGS[@]}")
   elif [[ "$FLAVOR" == "focal" ]]; then
     TAGS=("${FOCAL_TAGS[@]}")
+  elif [[ "$FLAVOR" == "jammy" ]]; then
+    TAGS=("${JAMMY_TAGS[@]}")
   else
-    echo "ERROR: unknown flavor - $FLAVOR. Must be either 'bionic' or 'focal'"
+    echo "ERROR: unknown flavor - $FLAVOR. Must be either 'bionic', 'focal', or 'jammy'"
     exit 1
   fi
 
@@ -112,9 +123,16 @@ publish_docker_manifest () {
   done
 }
 
+# Bionic
 publish_docker_images_with_arch_suffix bionic amd64
 publish_docker_manifest bionic amd64
 
+# Focal
 publish_docker_images_with_arch_suffix focal amd64
 publish_docker_images_with_arch_suffix focal arm64
 publish_docker_manifest focal amd64 arm64
+
+# Jammy
+publish_docker_images_with_arch_suffix jammy amd64
+publish_docker_images_with_arch_suffix jammy arm64
+publish_docker_manifest jammy amd64 arm64
