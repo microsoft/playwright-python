c990cb956553db56c60cb98fd4079fecd3ebe14b
Ross Wollman <rwoll@users.noreply.github.com>
2022-09-27 18:58:06 -0400

    chore: roll Playwright to 1.26.1 (#1570)

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ except ImportError:
     InWheel = None
 from wheel.bdist_wheel import bdist_wheel as BDistWheelCommand
 
-driver_version = "1.26.0"
+driver_version = "1.26.1"
 
 
 def extractall(zip: zipfile.ZipFile, path: str) -> None:
