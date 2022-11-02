5832fcf87c1480c161716d250d2bc97f2d61644b
Max Schmitt <max@schmitt.mx>
2022-11-02 09:49:04 -0700

    chore: bump greenlet to v2.0.0

--- setup.py
+++ setup.py
@@ -212,7 +212,7 @@ setup(
     packages=["playwright"],
     include_package_data=True,
     install_requires=[
-        "greenlet==1.1.3",
+        "greenlet==2.0.0",
         "pyee==8.1.0",
         "typing-extensions;python_version<='3.8'",
     ],
