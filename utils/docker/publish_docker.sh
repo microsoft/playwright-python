d87e105effeb0b656b6cfe8945fa207ca6c14a71
Max Schmitt <max@schmitt.mx>
2022-11-30 15:42:39 -0800

    chore: drop Ubuntu 18 bionic Docker images (#1667)

--- utils/docker/publish_docker.sh
+++ utils/docker/publish_docker.sh
@@ -31,29 +31,22 @@ if [[ -z "${GITHUB_SHA}" ]]; then
   exit 1
 fi
 
-BIONIC_TAGS=(
-  "next-bionic"
-)
-if [[ "$RELEASE_CHANNEL" == "stable" ]]; then
-  BIONIC_TAGS+=("bionic")
-fi
-
 FOCAL_TAGS=(
   "next"
   "sha-${GITHUB_SHA}"
   "next-focal"
 )
-
-JAMMY_TAGS=(
-  "next-jammy"
-)
-
 if [[ "$RELEASE_CHANNEL" == "stable" ]]; then
   FOCAL_TAGS+=("latest")
   FOCAL_TAGS+=("focal")
   FOCAL_TAGS+=("v${PW_VERSION}-focal")
   FOCAL_TAGS+=("v${PW_VERSION}")
+fi
 
+JAMMY_TAGS=(
+  "next-jammy"
+)
+if [[ "$RELEASE_CHANNEL" == "stable" ]]; then
   JAMMY_TAGS+=("jammy")
   JAMMY_TAGS+=("v${PW_VERSION}-jammy")
 fi
@@ -69,14 +62,12 @@ tag_and_push() {
 publish_docker_images_with_arch_suffix() {
   local FLAVOR="$1"
   local TAGS=()
-  if [[ "$FLAVOR" == "bionic" ]]; then
-    TAGS=("${BIONIC_TAGS[@]}")
-  elif [[ "$FLAVOR" == "focal" ]]; then
+  if [[ "$FLAVOR" == "focal" ]]; then
     TAGS=("${FOCAL_TAGS[@]}")
   elif [[ "$FLAVOR" == "jammy" ]]; then
     TAGS=("${JAMMY_TAGS[@]}")
   else
-    echo "ERROR: unknown flavor - $FLAVOR. Must be either 'bionic', 'focal', or 'jammy'"
+    echo "ERROR: unknown flavor - $FLAVOR. Must be either 'focal', or 'jammy'"
     exit 1
   fi
   local ARCH="$2"
@@ -97,14 +88,12 @@ publish_docker_images_with_arch_suffix() {
 publish_docker_manifest () {
   local FLAVOR="$1"
   local TAGS=()
-  if [[ "$FLAVOR" == "bionic" ]]; then
-    TAGS=("${BIONIC_TAGS[@]}")
-  elif [[ "$FLAVOR" == "focal" ]]; then
+  if [[ "$FLAVOR" == "focal" ]]; then
     TAGS=("${FOCAL_TAGS[@]}")
   elif [[ "$FLAVOR" == "jammy" ]]; then
     TAGS=("${JAMMY_TAGS[@]}")
   else
-    echo "ERROR: unknown flavor - $FLAVOR. Must be either 'bionic', 'focal', or 'jammy'"
+    echo "ERROR: unknown flavor - $FLAVOR. Must be either 'focal', or 'jammy'"
     exit 1
   fi
 
@@ -122,10 +111,6 @@ publish_docker_manifest () {
     docker manifest push "${BASE_IMAGE_TAG}"
   done
 }
-
-# Bionic
-publish_docker_images_with_arch_suffix bionic amd64
-publish_docker_manifest bionic amd64
 
 # Focal
 publish_docker_images_with_arch_suffix focal amd64
