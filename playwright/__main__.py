ac6e957bb0c77a1723ded5fa7635c36129e53ac7
Max Schmitt <max@schmitt.mx>
2024-04-03 17:41:41 +0200

    fix(cli): don't show a stack trace when command + c of the CLI (#2390)

--- playwright/__main__.py
+++ playwright/__main__.py
@@ -19,11 +19,14 @@ from playwright._impl._driver import compute_driver_executable, get_driver_env
 
 
 def main() -> None:
-    driver_executable, driver_cli = compute_driver_executable()
-    completed_process = subprocess.run(
-        [driver_executable, driver_cli, *sys.argv[1:]], env=get_driver_env()
-    )
-    sys.exit(completed_process.returncode)
+    try:
+        driver_executable, driver_cli = compute_driver_executable()
+        completed_process = subprocess.run(
+            [driver_executable, driver_cli, *sys.argv[1:]], env=get_driver_env()
+        )
+        sys.exit(completed_process.returncode)
+    except KeyboardInterrupt:
+        sys.exit(130)
 
 
 if __name__ == "__main__":
