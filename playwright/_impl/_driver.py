903b2cb9bca9b55fda4c56de216a27c52c0f413b
KRRT7 <106575910+KRRT7@users.noreply.github.com>
2024-05-24 13:43:52 +0000

    remove get_registry_directory

--- playwright/_impl/_driver.py
+++ playwright/_impl/_driver.py
@@ -22,31 +22,6 @@ import playwright
 from playwright._repo_version import version
 import pathlib
 
-def get_registry_directory() -> str:
-    env_defined = os.environ.get('PLAYWRIGHT_BROWSERS_PATH')
-    path_home = Path.home()
-    if env_defined == '0':
-        result = pathlib.Path(__file__).parent.parent.parent.joinpath('.local-browsers')
-    elif env_defined:
-        result = pathlib.Path(env_defined)
-    else:
-        cache_directory: Union[str, Path] = ''
-        if os.name == 'posix':
-            cache_directory = os.environ.get('XDG_CACHE_HOME', path_home / '.cache')
-        elif os.name == 'darwin':
-            cache_directory = path_home / 'Library' / 'Caches'
-        elif os.name == 'nt':
-            cache_directory = os.environ.get('LOCALAPPDATA', path_home / 'AppData' / 'Local')
-        else:
-            raise RuntimeError('Unsupported platform: ' + os.name)
-        result = pathlib.Path(cache_directory, 'ms-playwright')
-
-    if not result.is_absolute():
-        init_cwd = os.environ.get('INIT_CWD') or os.getcwd()
-        result = pathlib.Path(init_cwd).resolve().joinpath(result)
-
-    return str(result)
-
 
 def compute_driver_executable() -> Tuple[str, str]:
     driver_path = Path(inspect.getfile(playwright)).parent / "driver"
