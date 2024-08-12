2475081be2052f2dff21fc9f1f9f5fdf806c7874
Max Schmitt <max@schmitt.mx>
2024-08-12 13:58:26 +0200

    chore: add todo for waitFor* / browser.disconnected event ordering

--- playwright/_impl/_browser_type.py
+++ playwright/_impl/_browser_type.py
@@ -231,6 +231,13 @@ class BrowserType(ChannelOwner):
                     context._on_close()
                 browser._on_close()
             connection.cleanup(reason)
+            # TODO: Backport https://github.com/microsoft/playwright/commit/d8d5289e8692c9b1265d23ee66988d1ac5122f33
+            # Give a chance to any API call promises to reject upon page/context closure.
+            # This happens naturally when we receive page.onClose and browser.onClose from the server
+            # in separate tasks. However, upon pipe closure we used to dispatch them all synchronously
+            # here and promises did not have a chance to reject.
+            # The order of rejects vs closure is a part of the API contract and our test runner
+            # relies on it to attribute rejections to the right test.
 
         transport.once("close", handle_transport_close)
 
