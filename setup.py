64c739e3a5b9d849d2645d26721a7adbadc09527
Max Schmitt <max@schmitt.mx>
2024-09-09 16:01:45 +0200

    update driver

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ except ImportError:
     InWheel = None
 from wheel.bdist_wheel import bdist_wheel as BDistWheelCommand
 
-driver_version = "1.47.0"
+driver_version = "1.47.0-beta-1725889926000"
 
 
 def extractall(zip: zipfile.ZipFile, path: str) -> None:
