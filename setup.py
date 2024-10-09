a71a0ce3609fb8027c88cb26cdcb581fbfcabf9b
Max Schmitt <max@schmitt.mx>
2024-10-09 15:14:11 +0200

    chore: support Python 3.13 (#2589)

--- setup.py
+++ setup.py
@@ -233,6 +233,7 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
